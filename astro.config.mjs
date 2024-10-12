7a7e407ee647fdd08aed354f8559d4b6108ed442
jamesli2021 <95068031+jamesli2021@users.noreply.github.com>
2024-10-12 13:03:46 +0800

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import node from '@astrojs/node';
-
 import tailwind from '@astrojs/tailwind';
+
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-
-  adapter: node({
-  mode: 'standalone',
-}),
-
   integrations: [tailwind()],
+  output: 'server',
+  adapter: cloudflare()
 });
